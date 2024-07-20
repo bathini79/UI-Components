148f4350625569dd74240acf2b9e08c322152d88
Bathini Nipun <116077457+bathini79@users.noreply.github.com>
2024-07-20 18:16:55 +0000

    feat:ui components

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
+import { useState } from 'react';
 import './App.css';
+import Tabs from './Tabs';
+import Accordian from './Accordian';
+import Corousal from './Corousal';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+        <div>Tab</div>
+        <Tabs />
+        <div>Accordian</div>
+        <Accordian/>
+        <div>Corousal</div>
+        <Corousal/>
       </header>
     </div>
   );
